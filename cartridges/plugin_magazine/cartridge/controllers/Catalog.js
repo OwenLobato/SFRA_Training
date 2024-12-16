e6adbeeb0edccf8257d54919c1e92d6f50169ab3
OwenLobato <owenlobatov@gmail.com>
2024-12-16 13:08:35 -0600

    Create endpoint

---
+++ cartridges/plugin_magazine/cartridge/controllers/Catalog.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @namespace Catalog
+ */
+
+var server = require('server');
+
+/**
+ * Catalog-Show : The Catalog-Show endpoint will render the shopper's Catalog page. Once a shopper logs in they will see is a dashboard that displays profile, address, payment and order information.
+ * @name Base/Catalog-Show
+ * @function
+ * @memberof Account
+ * @param {middleware} - server.middleware.https
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+server.get('Show', function (req, res, next) {
+
+    var option = req.querystring.option || '1'; 
+    res.render('catalog/magazine', { option: option });
+
+    next();
+});
+
+module.exports = server.exports();
