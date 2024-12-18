c7bcbb0184c32b42c80c768cdb65da6662212b1d
OwenLobato <owenlobatov@gmail.com>
2024-12-18 10:11:33 -0600

    Add iframe

--- cartridges/cartridge_test/cartridge/controllers/Test.js
+++ cartridges/cartridge_test/cartridge/controllers/Test.js
@@ -35,7 +35,8 @@ server.replace(
 
         pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
 
-        var page = PageMgr.getPage('test-store');
+        // var page = PageMgr.getPage('test-store');
+        var page = null;
 
         if (page && page.isVisible()) {
             res.page('test-store');
@@ -46,5 +47,13 @@ server.replace(
     },
     pageMetaData.computedPageMetaData
 );
+
+server.get('iframe', function (req, res, next) {
+    // var Site = require('dw/system/Site');
+    // var urlFrame = Site.getCurrent().getCustomPreferenceValue('framePath');
+
+    res.render('test/frame', { urlFrame: 'https://corporativoramaconsulta.turbopacmx.com/Clientes.Consulta/' });
+    next();
+});
 
 module.exports = server.exports();
